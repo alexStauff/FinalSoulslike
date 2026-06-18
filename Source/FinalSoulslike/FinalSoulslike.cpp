d7f4562083e19c787057662386e6710e3b7cf0a9
Alex Stauff <stauffalex@gmail.com>
2026-06-18 19:52:58 -0400

    TheOtherInits

---
+++ Source/FinalSoulslike/FinalSoulslike.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FinalSoulslike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FinalSoulslike, "FinalSoulslike" );
+
+DEFINE_LOG_CATEGORY(LogFinalSoulslike)
